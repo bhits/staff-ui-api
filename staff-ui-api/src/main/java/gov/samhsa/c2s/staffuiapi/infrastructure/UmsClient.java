a46cd46b26a6b6a3e6a9c97cb683f2325a10a630
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-08-09 14:38:08 -0400

    Remove unused import and getting locale from context
    #14568 - As a developer I will provide I18n support to DB lookup values

--- staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/infrastructure/UmsClient.java
+++ staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/infrastructure/UmsClient.java
@@ -3,12 +3,7 @@ package gov.samhsa.c2s.staffuiapi.infrastructure;
 import gov.samhsa.c2s.staffuiapi.infrastructure.dto.PageableDto;
 import gov.samhsa.c2s.staffuiapi.infrastructure.dto.UmsUserDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/service/UmsLookupService.java
+++ staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/service/UmsLookupService.java
@@ -2,8 +2,6 @@ package gov.samhsa.c2s.staffuiapi.service;
 
 import gov.samhsa.c2s.staffuiapi.service.dto.UserCreationLookupDto;
 
-import java.util.Locale;
-
 public interface UmsLookupService {
     UserCreationLookupDto getUserCreationLookupInfo();
 }

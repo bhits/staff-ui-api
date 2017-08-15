d5a7516a54a4e381fe1cf221c6f8867c1cc06acb
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-08-15 15:19:32 -0400

    Add apache http client dependency, implement custom login exception and implement capturing and throwing the correct UAA login exception
    #14794 - Single UI 4: As a developer I will create a notification to user after 3 failed login attempts about account lockout.

--- staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/service/UaaServiceImpl.java
+++ staff-ui-api/src/main/java/gov/samhsa/c2s/staffuiapi/service/UaaServiceImpl.java
@@ -3,7 +3,8 @@ package gov.samhsa.c2s.staffuiapi.service;
 import gov.samhsa.c2s.staffuiapi.config.StaffUiApiProperties;
 import gov.samhsa.c2s.staffuiapi.infrastructure.UaaClient;
 import gov.samhsa.c2s.staffuiapi.service.dto.LoginRequestDto;
-import gov.samhsa.c2s.staffuiapi.service.exception.AuthenticationException;
+import gov.samhsa.c2s.staffuiapi.service.exception.AccountLockedException;
+import gov.samhsa.c2s.staffuiapi.service.exception.BadCredentialsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,8 @@ import java.util.Map;
 public class UaaServiceImpl implements UaaService {
     private static final String OAUTH2_GRAND_TYPE = "password";
     private static final String OAUTH2_RESPONSE_TYPE = "token";
+    private static final String BAD_CREDENTIAL_ERROR_MESSAGE = "Bad credentials";
+    private static final String ACCOUNT_LOCKED_ERROR_MESSAGE = "Your account has been locked because of too many failed attempts to login";
 
     private final StaffUiApiProperties staffUiApiProperties;
     private final UaaClient uaaClient;
@@ -39,8 +42,14 @@ public class UaaServiceImpl implements UaaService {
         try {
             return uaaClient.getTokenUsingPasswordGrant(requestParams);
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            throw new AuthenticationException(e);
+            String errorMessage = e.getMessage();
+            log.error(errorMessage);
+            if(errorMessage.contains(BAD_CREDENTIAL_ERROR_MESSAGE)){
+                throw new BadCredentialsException();
+            }else if(errorMessage.contains(ACCOUNT_LOCKED_ERROR_MESSAGE)){
+                throw new AccountLockedException();
+            }
         }
+        return null;
     }
 }
